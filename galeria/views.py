c5cf41088e3ec2d2d5141a6e3163ff21696c20bf
Mateus Toledo <mtoledobenks@gmail.com>
2025-06-09 22:11:52 -0300

    adicionando referencias na url do index

--- galeria/views.py
+++ galeria/views.py
@@ -1,5 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from galeria.models import fotografia   
+
 
 def index(request):
-    return render(request, 'galeria/index.html')
+    fotografias = fotografia.objects.all()
+    return render(request, 'galeria/index.html', {'cards': fotografias})
+
+def imagem(request, foto_id):
+    foto = get_object_or_404(fotografia, pk=foto_id)
+    return render(request,'galeria/imagem.html', {"fotografia": foto})
